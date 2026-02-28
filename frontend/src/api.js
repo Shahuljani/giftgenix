e01f8f23c80ad0cdc29b02bde64dc8673a5f0a17
Shahuljani <mrsjcreations1@gmail.com>
2026-02-28 06:13:45 +0530

    Connected to live backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:5000";
+const API = "https://giftgenix.onrender.com";
 
 export const generateResponse = (data) =>
   axios.post(`${API}/generate`, data);
